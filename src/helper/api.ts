b4c71b68b89fce30e8eb60453dce47a8bbd8a3b6
kilic2 <165295987+kilic2@users.noreply.github.com>
2026-01-09 18:05:25 +0300

    2

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,8 +1,6 @@
-// src/helper/api.ts dosyanın içi muhtemelen şöyle olmalı:
-
 import axios from "axios";
 
 export const api = axios.create({
-  // VITE_API_URL tanımlıysa onu kullan, yoksa localhost'u (geliştirme ortamı için) kullan
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  
+  baseURL: "https://api.evdekom.com.tr", 
 });
