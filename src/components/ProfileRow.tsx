1755666a2b86b571e7e9fb1499c47d10f8462bf8
kilic2 <165295987+kilic2@users.noreply.github.com>
2026-01-17 18:09:26 +0000

    ok

--- src/components/ProfileRow.tsx
+++ src/components/ProfileRow.tsx
@@ -84,20 +84,17 @@ export const ProfileRow = ({ fetchProfiles, profile, handleClick }: Props) => {
                 <TableCell>{profile.email}</TableCell>
                 <TableCell>{profile.profileType?.name}</TableCell>
 
-                <TableCell>
-                    <div className="flex flex-wrap gap-2">
-                        {profile.tags && profile.tags.length > 0 ? (
-                            profile.tags.map((tag: any, index: number) => (
-                                <Badge key={index} color="info">
-                                    {tag.name}
-                                </Badge>
-                            ))
-                        ) : (
-                            <span className="text-gray-400">-</span>
-                        )}
-                    </div>
-                </TableCell>
-
+                <TableCell className="max-w-xs"> {/* Set a max-width on the cell if you want to force wrapping */}
+                    <div className="whitespace-normal break-words">
+                {profile.tags && profile.tags.length > 0 ? (
+                      <span>
+                        {profile.tags.map((tag: any) => tag.name).join(', ')}
+                    </span>
+                     ) : (
+                     <span className="text-gray-400">-</span>
+                         )}
+                 </div>
+                    </TableCell>
                 <TableCell>
                     <div className="flex gap-2">
                         <div onClick={(e) => e.stopPropagation()}>
